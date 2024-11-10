eba4ab2d7ea338d8a5aa12c1f92d4e47af141e10
Rakesh Das <rakesh.das@digitalwoods.com>
2024-11-10 17:14:48 +0530

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'organic_honey_frontend',
-  webDir: 'www'
+  appName: 'school_mg_frontend',
+  webDir: 'www',
 };
 
 export default config;
