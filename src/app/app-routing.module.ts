ea8c5f1d5265918c99623cc7e5b8be65854fbf60
Rakesh Das <rakesh.das@digitalwoods.com>
2024-11-12 01:36:18 +0530

    gic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,31 +12,45 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () =>
       import('./app-pages/login/login.module').then((m) => m.LoginPageModule),
-  },  {
+  },
+  {
     path: 'home',
-    loadChildren: () => import('./app-pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./app-pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: 'signup',
-    loadChildren: () => import('./app-pages/signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () =>
+      import('./app-pages/signup/signup.module').then(
+        (m) => m.SignupPageModule
+      ),
   },
   {
     path: 'profile',
-    loadChildren: () => import('./app-pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./app-pages/profile/profile.module').then(
+        (m) => m.ProfilePageModule
+      ),
   },
   {
     path: 'product',
-    loadChildren: () => import('./app-pages/product/product.module').then( m => m.ProductPageModule)
+    loadChildren: () =>
+      import('./app-pages/product/product.module').then(
+        (m) => m.ProductPageModule
+      ),
   },
   {
     path: 'order',
-    loadChildren: () => import('./app-pages/order/order.module').then( m => m.OrderPageModule)
+    loadChildren: () =>
+      import('./app-pages/order/order.module').then((m) => m.OrderPageModule),
   },
   {
     path: 'subjects',
-    loadChildren: () => import('./app-pages/subjects/subjects.module').then( m => m.SubjectsPageModule)
+    loadChildren: () =>
+      import('./app-pages/subjects/subjects.module').then(
+        (m) => m.SubjectsPageModule
+      ),
   },
-
 ];
 
 @NgModule({
